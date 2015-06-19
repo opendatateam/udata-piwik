fed0b283a239993d33c3cf2109554032d678a001
David Larlet <david@larlet.fr>
2015-06-19 12:02:35 +0200

    Handle datasets and reuses publication events
    Refs etalab/udata#60

--- udata_piwik/tasks.py
+++ udata_piwik/tasks.py
@@ -2,10 +2,14 @@
 from __future__ import unicode_literals
 
 import logging
-
 from datetime import date, timedelta
+
+from flask import current_app
+
 from udata.tasks import connect, job
 from udata.api.signals import on_api_call
+from udata.core.dataset.signals import on_dataset_published
+from udata.core.reuse.signals import on_reuse_published
 
 import udata_piwik
 from .counter import counter
@@ -35,7 +39,31 @@ def piwik_yesterday_metrics(self):
 
 
 @connect(on_api_call)
-def piwik_track_url(url, **params):
-    '''Track a URL into Piwik.'''
+def piwik_track_api(url, **params):
+    '''Track an API request into Piwik.'''
     log.debug('Sending to piwik: {url}'.format(url=url))
+    udata_piwik.track(url, **params)
+
+
+@connect(on_dataset_published)
+def piwik_track_dataset_published(url, **params):
+    '''Track a dataset publication into Piwik.'''
+    log.debug('Sending to piwik: {url}'.format(url=url))
+    goals = current_app.config.get('PIWIK_GOALS')
+    if goals:
+        params.update({
+            'idgoal': goals['PUBLISH_DATASET'],
+        })
+    udata_piwik.track(url, **params)
+
+
+@connect(on_reuse_published)
+def piwik_track_reuse_published(url, **params):
+    '''Track a reuse publication into Piwik.'''
+    log.debug('Sending to piwik: {url}'.format(url=url))
+    goals = current_app.config.get('PIWIK_GOALS')
+    if goals:
+        params.update({
+            'idgoal': goals['PUBLISH_REUSE'],
+        })
     udata_piwik.track(url, **params)
