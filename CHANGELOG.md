cb7254ce15d098c7202a3d7a258a3b8193bf9678
Axel Haustant <noirbizarre@gmail.com>
2018-03-13 17:50:47 +0100

    Update to version 1.1.1.dev for next development cycle

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## Current (in progress)
+
+- Nothing yet
+
 ## 1.1.0 (2018-03-13)
 
 - Refactor `counter.handle_downloads` - fix [#1421](https://github.com/opendatateam/udata/issues/1421)
--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ tests_require = pip('test.pip')
 
 setup(
     name='udata-piwik',
-    version='1.1.0',
+    version='1.1.1.dev',
     description='Piwik support for uData',
     long_description=long_description,
     url='https://github.com/opendatateam/udata-piwik',
