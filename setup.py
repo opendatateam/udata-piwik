af268ed926d003c0d9d5c6441fa353a22a4bb7da
Stanislas P <57526019+quaxsze@users.noreply.github.com>
2020-05-12 16:36:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ tests_require = pip('test.pip')
 
 setup(
     name='udata-piwik',
-    version='2.0.0.dev',
+    version='2.0.3.dev',
     description='Piwik support for uData',
     long_description=long_description,
     long_description_content_type='text/markdown',
